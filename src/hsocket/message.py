5e6faac8261c223e35a57db99969f29808ec44b8
pdt012 <jxh0615@163.com>
2022-11-09 23:21:48 +0900

    Changed header format.

--- src/hsocket/message.py
+++ src/hsocket/message.py
@@ -3,7 +3,7 @@ from typing import Optional, Any
 import json
 
 
-class ProtoCode:
+class ContentType:
     NONE = 0x0  # 空报文
     ERROR_ = 0x1  # 错误报文
     HEADERONLY = 0x2  # 只含报头
@@ -16,47 +16,47 @@ class MessageConfig:
 
 
 class Header:
-    HEADER_LENGTH = 8
+    HEADER_LENGTH = 10
 
-    def __init__(self, protocode, opcode, statuscode, length):
-        self.protocode: int = protocode
-        self.opcode: int = opcode
-        self.statuscode: int = statuscode
-        self.length: int = length
+    def __init__(self, contenttype, opcode, statuscode, length):
+        self.contenttype: int = contenttype  # 报文内容码
+        self.opcode: int = opcode  # 操作码
+        self.statuscode: int = statuscode  # 状态码
+        self.length: int = length  # 报文长度
     
     def to_bytes(self) -> bytes:
         header = b""
-        header += self.protocode.to_bytes(1, 'little', signed=False)  # 协议码
-        header += self.opcode.to_bytes(1, 'little', signed=False)  # 操作码
+        header += self.contenttype.to_bytes(2, 'little', signed=False)  # 报文内容码
+        header += self.opcode.to_bytes(2, 'little', signed=False)  # 操作码
         header += self.statuscode.to_bytes(2, 'little', signed=False)  # 状态码
         header += self.length.to_bytes(4, 'little', signed=False)  # 报文长度
         return header
 
     @classmethod
-    def from_bytes(cls, data: bytes) -> Optional["Header"]:
+    def from_bytes(cls, data: bytes) -> Optional["Message.Header"]:
         if len(data) != cls.HEADER_LENGTH:
             return None
-        protocode = int(data[0])  # 协议码
-        opcode = int(data[1])  # 操作码
-        statuscode = int().from_bytes(data[2:4], 'little', signed=False)  # 响应码
-        length = int().from_bytes(data[4:8], 'little', signed=False)  # 文本内容起始位置
-        return Header(protocode, opcode, statuscode, length)
+        contenttype = int.from_bytes(data[0:2], 'little', signed=False)  # 报文内容码
+        opcode = int.from_bytes(data[2:4], 'little', signed=False)  # 操作码
+        statuscode = int().from_bytes(data[4:6], 'little', signed=False)  # 响应码
+        length = int().from_bytes(data[6:10], 'little', signed=False)  # 文本内容起始位置
+        return cls(contenttype, opcode, statuscode, length)
 
 
 class Message:
-    def __init__(self, protocode=ProtoCode.NONE, opcode=0, statuscode=0, content: str = ""):
-        self.__protocode: int = protocode
-        self.__opcode: int = opcode
-        self.__statuscode: int = statuscode
+    def __init__(self, contenttype=ContentType.NONE, opcode=0, statuscode=0, content: str = ""):
+        self.__contenttype: int = contenttype  # 报文内容码
+        self.__opcode: int = opcode  # 操作码
+        self.__statuscode: int = statuscode  # 响应码
         self.__content: str = ""
         self.__json: dict = {}
         
         if content:
-            if protocode == ProtoCode.HEADERONLY:
+            if contenttype == ContentType.HEADERONLY:
                 pass
-            elif protocode == ProtoCode.PLAINTEXT:
+            elif contenttype == ContentType.PLAINTEXT:
                 self.__content = content
-            elif protocode == ProtoCode.JSONOBJRCT:
+            elif contenttype == ContentType.JSONOBJRCT:
                 self.__content = content
                 self.__json = json.loads(content)
     
@@ -64,23 +64,23 @@ class Message:
     def HeaderContent(cls, header: Header, content: str) -> "Message":
         if header == None:
             return Message()
-        msg = Message(header.protocode, header.opcode, header.statuscode, content)
+        msg = Message(header.contenttype, header.opcode, header.statuscode, content)
         return msg
 
     @classmethod
     def HeaderOnlyMsg(cls, opcode=0, statuscode=0):
-        msg = Message(ProtoCode.HEADERONLY, opcode, statuscode)
+        msg = Message(ContentType.HEADERONLY, opcode, statuscode)
         return msg
         
     @classmethod
     def PlainTextMsg(cls, opcode=0, statuscode=0, text: str = ""):
-        msg = Message(ProtoCode.PLAINTEXT, opcode, statuscode)
+        msg = Message(ContentType.PLAINTEXT, opcode, statuscode)
         msg.__content = text
         return msg
         
     @classmethod
     def JsonMsg(cls, opcode=0, statuscode=0, dict_: dict = None, **kw):
-        msg = Message(ProtoCode.JSONOBJRCT, opcode, statuscode)
+        msg = Message(ContentType.JSONOBJRCT, opcode, statuscode)
         if dict_ is not None:
             msg.__json = dict_
         for key in kw.keys():
@@ -89,7 +89,7 @@ class Message:
         return msg
 
     def isValid(self):
-        return self.__protocode != ProtoCode.NONE and self.__protocode != ProtoCode.ERROR_
+        return self.__contenttype != ContentType.NONE and self.__contenttype != ContentType.ERROR_
 
     def get(self, key: str) -> Any:
         ret = self.__json.get(key)
@@ -98,9 +98,9 @@ class Message:
     def content(self) -> str:
         return self.__content
 
-    def protocode(self) -> int:
-        """获取协议码"""
-        return self.__protocode
+    def contenttype(self) -> int:
+        """获取报文内容码"""
+        return self.__contenttype
         
     def opcode(self) -> int:
         """获取操作码"""
@@ -111,30 +111,30 @@ class Message:
         return self.__statuscode
         
     def to_bytes(self) -> bytes:
-        if self.__protocode == ProtoCode.HEADERONLY:
+        if self.__contenttype == ContentType.HEADERONLY:
             content = b""
-        elif self.__protocode == ProtoCode.PLAINTEXT:
+        elif self.__contenttype == ContentType.PLAINTEXT:
             content = self.__content.encode(MessageConfig.ENCODING)
-        elif self.__protocode == ProtoCode.JSONOBJRCT:
+        elif self.__contenttype == ContentType.JSONOBJRCT:
             content = json.dumps(self.__json).encode(MessageConfig.ENCODING)
         else:
             content = self.__content.encode(MessageConfig.ENCODING)
         length = len(content)  # 数据包长度(不包含报头)
-        header = Header(self.__protocode, self.__opcode, self.__statuscode, length)
+        header = Header(self.__contenttype, self.__opcode, self.__statuscode, length)
         return header.to_bytes() + content
 
     @classmethod
     def from_bytes(cls, data: bytes):
         if len(data) < Header.HEADER_LENGTH:
             return Message()
-        header = Header.from_bytes(data[0:8])
+        header = Header.from_bytes(data[0:Header.HEADER_LENGTH])
         if header == None:
             return Message()
-        msg = Message.HeaderContent(header, data[8:].decode(MessageConfig.ENCODING))
+        msg = Message.HeaderContent(header, data[Header.HEADER_LENGTH:].decode(MessageConfig.ENCODING))
         return msg
 
     def __str__(self):
-        return (f"pro:{self.__protocode}  op:{self.__opcode}  sta:{self.__statuscode}\n"
+        return (f"pro:{self.__contenttype}  op:{self.__opcode}  sta:{self.__statuscode}\n"
                 f"content:\n{self.__content}")
     
     def __repr__(self):
